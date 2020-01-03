745b3b9f9f0365ff5f2d299d695528252b49a083
tryao <yaotairan@gmail.com>
2020-01-03 13:10:49 +0800

    feat: Allow leaf hot restart

--- leaf.go
+++ leaf.go
@@ -3,16 +3,31 @@ package leaf
 import (
 	"os"
 	"os/signal"
+	"syscall"
 
 	"github.com/YiuTerran/leaf/console"
 	"github.com/YiuTerran/leaf/log"
 	"github.com/YiuTerran/leaf/module"
 )
 
-func Run(consolePort int, mods ...module.Module) {
+var (
+	c = make(chan os.Signal, 1)
+)
+
+//手动关闭服务
+func CloseServer() {
+	c <- os.Interrupt
+}
+
+//重启服务
+func RestartServer() {
+	c <- syscall.SIGUSR1
+}
+
+func Run(consolePort int, getMods func() []module.Module) {
 	//注意在此之前要调用log.InitLogger
 	log.Info("Leaf %v starting up", version)
-
+	mods := getMods()
 	// module
 	for i := 0; i < len(mods); i++ {
 		module.Register(mods[i])
@@ -21,10 +36,14 @@ func Run(consolePort int, mods ...module.Module) {
 	// console
 	console.Init(consolePort)
 	// close
-	c := make(chan os.Signal, 1)
 	signal.Notify(c, os.Interrupt, os.Kill)
 	sig := <-c
-	log.Info("Leaf closing down (signal: %v)", sig)
 	console.Destroy()
 	module.Destroy()
+	if sig == syscall.SIGUSR1 {
+		log.Info("Leaf hot restarting...")
+		Run(consolePort, getMods)
+	} else {
+		log.Info("Leaf closing down (signal: %v)", sig)
+	}
 }
