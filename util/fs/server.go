2266f903aa82e68d43b66d0540f7475433084855
tryao <yaotairan@gmail.com>
2020-01-03 14:00:57 +0800

    feat: add static file server rule

---
+++ util/fs/server.go
@@ -0,0 +1,31 @@
+package fs
+
+import (
+	"net/http"
+	"strings"
+)
+
+//加强版静态文件服务
+//如果打开文件夹里面有index.html则返回内容，否则直接返回文件
+type NeuteredFileSystem struct {
+	fs http.FileSystem
+}
+
+func (nfs NeuteredFileSystem) Open(path string) (http.File, error) {
+	f, err := nfs.fs.Open(path)
+	if err != nil {
+		return nil, err
+	}
+
+	s, err := f.Stat()
+	if err != nil {
+		return nil, err
+	}
+	if s.IsDir() {
+		index := strings.TrimSuffix(path, "/") + "/index.html"
+		if _, err := nfs.fs.Open(index); err != nil {
+			return nil, err
+		}
+	}
+	return f, nil
+}
