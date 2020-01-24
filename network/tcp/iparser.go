f06b3576193abfc8126bdf3e5574581604b4363f
tryao <yaotairan@gmail.com>
2020-01-24 20:40:59 +0800

    feat: add directly write func for tcp

--- network/tcp/iparser.go
+++ network/tcp/iparser.go
@@ -6,3 +6,20 @@ type IParser interface {
 	Read(conn *Conn) ([]byte, error)
 	Write(conn *Conn, args ...[]byte) error
 }
+
+//直接写入
+func DirectlyWrite(conn *Conn, args ...[]byte) error {
+	var msgLen uint32
+	for i := 0; i < len(args); i++ {
+		msgLen += uint32(len(args[i]))
+	}
+
+	msg := make([]byte, msgLen)
+	var l int
+	for i := 0; i < len(args); i++ {
+		copy(msg[l:], args[i])
+		l += len(args[i])
+	}
+	conn.Write(msg)
+	return nil
+}
