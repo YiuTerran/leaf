546fd76f74fbe265cebdadd1d7d9beb93969be4d
tryao <yaotairan@gmail.com>
2020-04-15 10:44:09 +0800

    fix: compress image error

--- util/image/convert.go
+++ util/image/convert.go
@@ -4,9 +4,12 @@ import (
 	"bufio"
 	"bytes"
 	"encoding/base64"
+	"strings"
 
 	"github.com/disintegration/imaging"
 )
+
+const base64Header = ";base64,"
 
 //压缩图片到指定格式
 //保持宽高比
@@ -24,10 +27,24 @@ func Compress(data []byte, width int, format imaging.Format) ([]byte, error) {
 	return buf.Bytes(), nil
 }
 
+func RemoveImgBase64Header(img string) string {
+	if idx := strings.Index(img, base64Header); idx >= 0 {
+		img = strings.TrimPrefix(img, img[:idx+8]) //8是header的长度
+	}
+	return img
+}
+
+func AddImgBase64Header(img string) string {
+	if strings.HasPrefix(img, "data:image") {
+		return img
+	}
+	return "data:image/jpeg;base64," + img
+}
+
 func ConvertToBase64(img []byte) string {
-	return base64.StdEncoding.EncodeToString(img)
+	return AddImgBase64Header(base64.StdEncoding.EncodeToString(img))
 }
 
 func ConvertToImage(data string) ([]byte, error) {
-	return base64.StdEncoding.DecodeString(data)
+	return base64.StdEncoding.DecodeString(RemoveImgBase64Header(data))
 }
